6156ca833e484d3741a2600796da022b938c8c64
xertifiedloaded <certifiedloaded@yahoo.com>
2024-03-08 12:30:09 +0100

    my changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Footer() {
     const getFullYear = new Date().getFullYear()
     return (
-        <div style={{textAlign:"center"}}>Makinde Olaitan's Portfolio {getFullYear}</div>
+        <div style={{textAlign:"center",marginBottom:"30px"}}>Makinde Olaitan's Portfolio {getFullYear}</div>
     )
 }
